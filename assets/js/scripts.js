426425a7743beb487e629044a131edeaaae77cb9
Your Name <you@example.com>
2018-12-07 11:58:00 -0800

    new website code .js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,16 +6,17 @@ $('.burger, .overlay').click(function(){
   $('body').toggleClass('overflow');
 });
 
-$(function(){
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
+
+// $(function(){
+// 		var selectedClass = "";
+// 		$(".fil-cat").click(function(){ 
+// 		selectedClass = $(this).attr("data-rel"); 
+//      $("#portfolio").fadeTo(100, 0.1);
+// 		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+//     setTimeout(function() {
+//       $("."+selectedClass).fadeIn().addClass('scale-anm');
+//       $("#portfolio").fadeTo(300, 1);
+//     }, 300); 
 		
-	});
-});
+// 	});
+// });
