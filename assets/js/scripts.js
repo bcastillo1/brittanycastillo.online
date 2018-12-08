7e259a080e9c4891fad0baeabdc72b1400e0f574
Your Name <you@example.com>
2018-12-08 13:11:25 -0800

    mobile nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,21 @@
-// Aaron Taylor
 $('.menu-btn').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('menu-btn_active'); $(".menu-nav").toggleClass('menu-nav_active');
   });
   
   
-
-
-$(function(){
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
-		
-	});
-});
+  
+  
+  $(function(){
+      var selectedClass = "";
+      $(".fil-cat").click(function(){ 
+      selectedClass = $(this).attr("data-rel"); 
+       $("#portfolio").fadeTo(100, 0.1);
+      $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+      setTimeout(function() {
+        $("."+selectedClass).fadeIn().addClass('scale-anm');
+        $("#portfolio").fadeTo(300, 1);
+      }, 300); 
+      
+    });
+  });
